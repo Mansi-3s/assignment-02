ac54eb4c62415085570072c48f0cd1146749f574
Mansi-3s <lalimagupta17@gmail.com>
2026-02-12 15:21:11 +0530

    Delete m1.py

--- m1.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-city = input("Enter your city: ")
-if age > 18:
-    print(f"Hello {name}, you are eligible to vote in {city}.")
-else:
-    print(f"Sorry {name}, you are too young.")
